5933bfd801e6395a72e668c7e6d87bdc97330512
LordNtlaloe <ntlal0e182@gmail.com>
2025-09-17 11:31:37 +0200

    Basic Login Screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, TextInput, View, Button, TouchableOpacity,Pressable } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Sign In</Text>
+      <TextInput style={styles.input} placeholder='Email' placeholderTextColor={"#FFFFFF"} />
+      <TextInput style={styles.input} placeholder='Password' secureTextEntry placeholderTextColor={"#FFFFFF"}/>
+        <Pressable onPress={() => {console.log("Button Pressed")}} style={styles.button}>
+          <Text style={styles.buttonText}>Sign In</Text>
+        </Pressable>
     </View>
   );
 }
@@ -13,8 +16,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#101010',
     justifyContent: 'center',
+    padding: 20,
+    gap: 20,
+    color: "#FFFFFF"
   },
+  input: {
+    borderWidth: 1,
+    borderColor: "#CCCCCC",
+    padding: 10,
+    borderRadius: 5,
+    color: "#FFFFFF",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "600",
+    color: "#FFFFFF"
+  },
+  button: {
+    backgroundColor: "#FF0101",
+    padding: 15,
+    borderRadius: 10
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+    fontWeight: "bold"
+  }
 });
